7460d7c18fe617864abb84adbb2d71df3ce977a0
andrew <andrew.brownbill@gmail.com>
2021-04-07 07:16:15 -0400

    Split the front end out from main.cpp

---
+++ pidsim_utils.h
@@ -0,0 +1,33 @@
+#ifndef __PIDSIM_UTILS__
+#define __PIDSIM_UTILS__
+
+#include <math.h>
+
+//
+// Some helper/ utility functions
+//
+
+///
+/// @brief Convert Degrees to Radians
+/// 
+/// @param[in] degrees - An angle in degrees
+/// @return            - Same angle in radians
+///
+inline double degToRad( double degrees ) 
+{
+  return degrees / 180.0 * M_PI; 
+}
+
+///
+/// @brief Convert Radians to Degrees
+/// 
+/// @param[in] radians - An angle in radians
+/// @return            - Same angle in degrees
+///
+inline double radToDeg( double radians ) 
+{
+  return radians / M_PI * 180.0;
+}
+
+#endif
+
